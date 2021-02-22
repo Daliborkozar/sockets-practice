c1889b18db3262dd767b871affa5d56be0c2f800
daliborkozar <kozardalibor@gmail.com>
2021-02-22 21:06:16 +0100

    first commit initializastion

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+//https://socket.io/get-started/chat
+
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+io.on("connection", function(socket) {
+    console.log('a user connected')
+//   socket.on("new-operations", function(data) {
+//     io.emit("new-remote-operations", data);
+//   });
+});
+
+http.listen(4000, function() {
+  console.log("listening on *:4000");
+});
